18c318c18d86c2e19f2a58560d6890b0ea8e57bb
RevitalS <revitle@gmail.com>
2016-06-09 02:50:48 +0300

    first page

--- app/src/main/java/com/example/revit/atry/MainActivity.java
+++ app/src/main/java/com/example/revit/atry/MainActivity.java
@@ -1,9 +1,7 @@
-package com.example.ex04db;
+package com.example.revit.atry;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
@@ -18,17 +17,20 @@ import android.widget.ViewSwitcher;
 public class MainActivity extends AppCompatActivity {
 
     String textSwitcherText[] = {"TextSwitcher Example", "Next Text in TextSwitcher", "Android TextSwitcher", "TextSwitcher Tutorial", "TextSwitcher in Android"};
-
+    Button btnNext;
     TextSwitcher textSwitcher;
 
     int switcherText = textSwitcherText.length;
-    int counter = -1;
+
+    int currentIndex=-1;
+    int messageCount=textSwitcherText.length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//go to layout file and bring me activitymain file
-      /*textSwitcher = (TextSwitcher) findViewById(R.id.text);
+        /*btnNext=(Button)findViewById(R.id.buttonNext);
+        textSwitcher = (TextSwitcher) findViewById(R.id.textS);
 
         textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
@@ -46,9 +48,22 @@ public class MainActivity extends AppCompatActivity {
         Animation animationIn = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
 
         textSwitcher.setOutAnimation(animationOut);
-        textSwitcher.setInAnimation(animationIn);*/
-    }
+        textSwitcher.setInAnimation(animationIn);
+        // ClickListener for NEXT button
+        // When clicked on Button TextSwitcher will switch between texts
+        // The current Text will go OUT and next text will come in with specified animation
+        btnNext.setOnClickListener(new View.OnClickListener() {
 
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                currentIndex++;
+                // If index reaches maximum reset it
+                if(currentIndex==messageCount)
+                    currentIndex=0;
+                textSwitcher.setText(textSwitcherText[currentIndex]);
+            }
+        });*/
+    }
 
     }
 

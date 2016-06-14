906bac250e1f4f943c4ea69fe81e4576a640ecfb
galit1321 <galit1321@gmail.com>
2016-06-14 17:56:07 +0300

    style and hide action bar

--- app/src/main/java/com/example/revit/atry/MainActivity.java
+++ app/src/main/java/com/example/revit/atry/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.revit.atry;
 
 import android.content.Intent;
-import android.graphics.Color;
+
 import android.os.Bundle;
 
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextSwitcher;
-import android.widget.TextView;
-import android.widget.ViewSwitcher;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,20 +16,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+      getSupportActionBar().hide();
         setContentView(R.layout.activity_main);//go to layout file and bring me activitymain file
         Handler h = new Handler(){
             @Override
             public void handleMessage(Message msg) {
-
                 Intent i=new Intent(MainActivity.this,LoginActivity.class);//move to the explition window
                 startActivity(i);
             }
         };
-
         h.sendEmptyMessageDelayed(0, 5000); // wait  seconds and move to next page
-
-
     }
 
 

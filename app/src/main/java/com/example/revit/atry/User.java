07a555acc7319a2dc68f01da83032325d2a3c701
galit1321 <galit1321@gmail.com>
2016-06-19 10:31:35 +0300

    commit user

---
+++ app/src/main/java/com/example/revit/atry/User.java
@@ -0,0 +1,28 @@
+package com.example.revit.atry;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+class User{
+    private String name;
+    private String username;
+    private String password;
+    private String icon;
+    private String email;
+
+    public User(JSONObject object){
+        try {
+            this.name = object.getString("Name");
+            this.password= object.getString("pass");
+            this.icon=object.getString("icon");
+            this.username=object.getString("username");
+            this.email=object.getString("email");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}

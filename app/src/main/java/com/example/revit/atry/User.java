b164476bc43b49904509fc28eb203db2c533978c
galit1321 <galit1321@gmail.com>
2016-06-20 19:49:23 +0300

    register add function to create conn to server but for now have truble a bit

--- app/src/main/java/com/example/revit/atry/User.java
+++ app/src/main/java/com/example/revit/atry/User.java
@@ -10,6 +10,8 @@ public class User{
     private String icon;
     private String email;
 
+
+    public  User(){}
     public User(JSONObject object){
         try {
             this.name = object.getString("Name");
@@ -25,4 +27,39 @@ public class User{
     public String getName() {
         return name;
     }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }

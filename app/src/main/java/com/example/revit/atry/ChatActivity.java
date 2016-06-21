a4dfca290c9b5b0d9e341f4daf7994bcd262a59e
RevitalS <revitle@gmail.com>
2016-06-21 18:28:47 +0300

    add edittext to msn activity

---
+++ app/src/main/java/com/example/revit/atry/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.revit.atry;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+        setContentView(R.layout.activity_chat);
+    }
+}

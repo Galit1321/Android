946ea25af5802d189c98244106ba6174616c2e68
galit1321 <galit1321@gmail.com>
2016-06-23 15:10:35 +0300

    service

--- app/src/main/java/com/example/revit/atry/MyService.java
+++ app/src/main/java/com/example/revit/atry/MyService.java
@@ -1,6 +1,7 @@
 package com.example.revit.atry;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.util.GregorianCalendar;
@@ -27,9 +29,10 @@ public class MyService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {return  0;}
+}
 
-        Thread thread = new Thread(new Runnable() {
+    /*    Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < 3; i++) {
@@ -39,12 +42,32 @@ public class MyService extends Service {
 
                     try {
                         Thread.sleep(300000);
+
+
+                        new NotificationCompat.Builder(MyService.this)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("My notification")
+                                .setContentText("ttt");
+                        //specifying an action and its category to be triggered once clicked on the notification
+                        Intent resultIntent = new Intent(MyService.this, ChatActivity.class);
+                        resultIntent.setAction("android.intent.action.MAIN");
+                        resultIntent.addCategory("android.intent.category.LAUNCHER");
+
+                        PendingIntent resultPendingIntent = PendingIntent.getActivity(MyService.this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        //building the notification
+                        mBuilder.setContentIntent(resultPendingIntent);
+                        int mNotificationId = 001;
+                        NotificationManager mNotifyMgr =
+                                (NotificationManager) MyService.this.getSystemService(MyService.this.NOTIFICATION_SERVICE);
+                        mNotifyMgr.notify(mNotificationId, mBuilder.build());
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                }
-                stopSelf();
-            }
+                }*/
+               // stopSelf();
+
+     /*      }
         });
 
         thread.start();
@@ -56,6 +79,6 @@ public class MyService extends Service {
     private void getAccelerometer(SensorEvent event) {
 
         }
-    }
+    }*/
 
 
